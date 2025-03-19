0ecd9b139fb26b161cba34ad8991298fe663552b
immersedreality <jeffreyeugenehoch@gmail.com>
2025-03-18 20:55:05 -0400

    Initial commit yo

---
+++ Punch-Line/Resources/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Punch-Line
+//
+//  Created by Jeffrey Eugene Hoch on 3/18/25.
+//
+
+import SwiftUI
+
+struct PunchLineColors {
+    static let pink = Color(red: 217.0, green: 120.0, blue: 127.0)
+}
