a3730e1bd332b1ec726a87a1d027df209820f21d
immersedreality <jeffreyeugenehoch@gmail.com>
2025-03-18 20:22:10 -0400

    Initial Commit

---
+++ Punch-Line/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Punch-Line
+//
+//  Created by Jeffrey Eugene Hoch on 3/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Punch-Line/Punch_LineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Punch_LineApp.swift
+//  Punch-Line
+//
+//  Created by Jeffrey Eugene Hoch on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Punch_LineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
